b23d67748f9e08c18660865fd3c76782d23374d4
loragabrielle <laleyble3240ant@student.fatima.edu.ph>
2026-04-10 22:28:11 +0800

    yes commit

---
+++ ScriptProducts.js
@@ -0,0 +1,10 @@
+// =============================================
+//  CSSP Garments Trading – script.js
+//  Clean, well-commented Vanilla JS
+// =============================================
+
+
+// ===== HELPER: grab elements easily =====
+const $ = id => document.getElementById(id);
+const $$ = sel => document.querySelectorAll(sel);
+
